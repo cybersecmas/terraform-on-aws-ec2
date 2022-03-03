5978fd61896709681f6766783a178606d96fe43e
HungTD <hungtd@gmail.com>
2022-03-03 11:26:12 +0700

    Added network config

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -18,3 +18,16 @@ Note-1:  AWS Credentials Profile (profile = "default") configured on your local 
 $HOME/.aws/credentials
 */
 
+# module local variables
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,9 +1,32 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
+
+resource "aws_network_interface" "ec2demo" {
+  subnet_id   = aws_subnet.public_a.id
+  private_ips = ["10.1.1.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami           = "ami-0742b4e673072066f"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
   user_data     = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.ec2demo.id
+    device_index         = 0
   }
 }
