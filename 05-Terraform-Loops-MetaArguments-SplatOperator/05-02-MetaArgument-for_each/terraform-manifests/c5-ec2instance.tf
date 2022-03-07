7abc8c4a9c80e455d4cd13039fbfc3c5ec6722f5
Hung Tran <hungtd@gmail.com>
2022-03-07 15:51:19 +0700

    step05 done

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c5-ec2instance.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c5-ec2instance.tf
@@ -9,14 +9,17 @@ data "aws_availability_zones" "my_azones" {
 
 # EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.amzlinux2.id
+  ami           = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
-  user_data = file("${path.module}/app1-install.sh")
-  key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  user_data     = file("${path.module}/app1-install.sh")
+  key_name      = var.instance_keypair
+
+  subnet_id = aws_subnet.public_a.id
+
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   # Create EC2 Instance in all Availabilty Zones of a VPC  
-  for_each = toset(data.aws_availability_zones.my_azones.names)
-  availability_zone = each.key  # You can also use each.value because for list items each.key == each.value
+  for_each          = toset(data.aws_availability_zones.my_azones.names)
+  availability_zone = each.key # You can also use each.value because for list items each.key == each.value
   tags = {
     "Name" = "for_each-Demo-${each.value}"
   }
