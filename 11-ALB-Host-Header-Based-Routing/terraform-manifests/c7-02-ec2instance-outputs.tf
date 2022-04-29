f03086eed25057f35b14bcba05d90f11241920ed
Hung Tran <hungtd@gmail.com>
2022-04-29 16:06:42 +0700

    done

--- 11-ALB-Host-Header-Based-Routing/terraform-manifests/c7-02-ec2instance-outputs.tf
+++ 11-ALB-Host-Header-Based-Routing/terraform-manifests/c7-02-ec2instance-outputs.tf
@@ -10,31 +10,29 @@ output "ec2_bastion_public_instance_ids" {
 ## ec2_bastion_public_ip
 output "ec2_bastion_public_ip" {
   description = "List of public IP addresses assigned to the instances"
-  value       = module.ec2_public.public_ip 
+  value       = module.ec2_public.public_ip
 }
 
 # App1 - Private EC2 Instances
 ## ec2_private_instance_ids
 output "app1_ec2_private_instance_ids" {
   description = "List of IDs of instances"
-  value       = module.ec2_private_app1.id
+  value       = module.ec2_private_app1 # this contains 2 instances "one" and "two"
 }
 ## ec2_private_ip
 output "app1_ec2_private_ip" {
   description = "List of private IP addresses assigned to the instances"
-  value       = module.ec2_private_app1.private_ip 
+  value       = module.ec2_private_app1 # this contains 2 instances "one" and "two"
 }
 
 # App2 - Private EC2 Instances
 ## ec2_private_instance_ids
 output "app2_ec2_private_instance_ids" {
   description = "List of IDs of instances"
-  value       = module.ec2_private_app2.id
+  value       = module.ec2_private_app2 # this contains 2 instances "one" and "two"
 }
 ## ec2_private_ip
 output "app2_ec2_private_ip" {
   description = "List of private IP addresses assigned to the instances"
-  value       = module.ec2_private_app2.private_ip 
+  value       = module.ec2_private_app2 # this contains 2 instances "one" and "two"
 }
-
-
